f87c70186b169819c4ee885781090530fa7a2464
Daniel J. Tenório <obliviatedan@protonmail.com>
2021-01-18 14:59:06 -0300

    Aula JavaScript

---
+++ Bootcamp-START_UNI_2021/Javascript/script.js
@@ -0,0 +1,38 @@
+
+/*
+var idade = prompt("Qual sua idade");
+if (idade > 18){
+    alert("Maior de idade!");
+}else{
+    alert("Menor de idade!");
+};
+*/
+
+/*
+var frutas = {nome:"maça", cor:"verde"}
+console.log(frutas.nome);
+alert(frutas.cor);
+*/
+
+/*var lista = ["maça", "pera", " laranja"];
+lista.push("uva");
+lista.pop();
+*/
+//console.log(lista);
+//console.log(lista.toSting());
+//console.log(lista.join( " - "));
+
+
+/*
+var nome = "Daniel Tenorio";
+var n1 = 5;
+var n2 = 3;
+var frase = "Feijão por cima do arroz";
+alert (nome + " tem " + idade + " anos ");
+alert (idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
+alert(frase.replace("por cima", "de ladinho"));
+*/
+
